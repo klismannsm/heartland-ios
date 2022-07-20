88356b063c31f6456ffdeba8e80e4b1a467c8d87
Darren Jaworski <darren.jaworski@e-hps.com>
2022-07-20 11:52:50 -0500

    remove imports to heartland_ios_sdk

--- Pod/Classes/Terminals/HpsConnectionConfig.m
+++ Pod/Classes/Terminals/HpsConnectionConfig.m
@@ -1,5 +1,4 @@
 #import "HpsConnectionConfig.h"
-#import <Heartland_iOS_SDK/Heartland_iOS_SDK-Swift.h>
 
 @implementation HpsConnectionConfig
 
--- Pod/Classes/Terminals/HpsTerminalResponse.m
+++ Pod/Classes/Terminals/HpsTerminalResponse.m
@@ -1,6 +1,5 @@
 #import "HpsTerminalResponse.h"
 #import "HpsHpaSharedParams.h"
-#import <Heartland_iOS_SDK/Heartland_iOS_SDK-Swift.h>
 
 @implementation HpsTerminalResponse
 
